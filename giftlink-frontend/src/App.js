bc5bb7b2b5cf7834a00a438ccdad4154cba1f8be
raynerbeta <33467773+raynerbeta@users.noreply.github.com>
2024-05-20 11:22:58 -0400

    Final updates

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,6 +6,8 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
 function App() {
     useNavigate();
     return (
@@ -16,6 +18,8 @@ function App() {
                 <Route path="/app" element={<MainPage />} />
                 <Route path="/app/login" element={<LoginPage />} />
                 <Route path="/app/register" element={<RegisterPage />} />
+                <Route path="/app/search" element={<SearchPage />} />
+                <Route path="/app/product/:productId" element={<DetailsPage />} />
             </Routes>
         </>
     );
